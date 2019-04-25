fe1a78fe97a99461915153dd8ffcdae98b7d30ae
Haley D'Amelio <dameliha@dukes.jmu.edu>
2019-04-25 13:54:19 -0400

    newmessaging

--- Project/NewMessage.aspx.cs
+++ Project/NewMessage.aspx.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class NewMessage : System.Web.UI.Page
 {
+    SqlConnection localDB = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localDB"].ConnectionString);
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -14,11 +19,21 @@ public partial class NewMessage : System.Web.UI.Page
 
     protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
     {
-       
+        int selected = GridView1.SelectedIndex;
+
+        GridViewRow row = GridView1.SelectedRow;
+        mainViewContactSchool.InnerText = row.Cells[1].Text;
+
+
     }
 
     protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
     {
+        
+    }
 
+    protected void ChangeMain_Click(object sender, EventArgs e)
+    {
+        
     }
 }
