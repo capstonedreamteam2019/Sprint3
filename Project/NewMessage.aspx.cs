8cd29a4f9fd8866ac765ecee863a16cf6bbd2cf5
Haley D'Amelio <dameliha@dukes.jmu.edu>
2019-04-25 16:55:26 -0400

    messages

--- Project/NewMessage.aspx.cs
+++ Project/NewMessage.aspx.cs
@@ -19,17 +19,21 @@ public partial class NewMessage : System.Web.UI.Page
 
     protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
     {
-        int selected = GridView1.SelectedIndex;
 
         GridViewRow row = GridView1.SelectedRow;
         mainViewContactSchool.InnerText = row.Cells[1].Text;
+       
+
+        GridView1.SelectedRowStyle.CssClass = "bg-primary text-white";
 
 
     }
 
     protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
     {
-        
+
+        GridViewRow row = GridView2.SelectedRow;
+        mainViewContactSchool.InnerText = row.Cells[1].Text;
     }
 
     protected void ChangeMain_Click(object sender, EventArgs e)
